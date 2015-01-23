7f80525b17113ffefda63b2ae9565a93819a60b1
chick <chick@qrhino.com>
2015-01-22 23:07:38 -0800

    fixed test_box but not very happy with it overall

--- test/test_box.py
+++ test/test_box.py
@@ -12,13 +12,13 @@ from hpgmg.finite_volume.boundary_condition import BoundaryCondition
 class TestBox(unittest.TestCase):
     def test_create(self):
         level = Level(64, 1, 1, 64, BoundaryCondition.DIRICHLET, 0, 1)
-        box = Box(level, Coord(0, 0, 0), 1, 2, 1)
+        box = Box(level, Coord(0, 0, 0), 20, 1, 1)
         self.assertTrue(box is not None)
         self.assertEqual(box.j_stride, 3)
         self.assertEqual(box.i_stride, 9)
 
-        box.vectors[50] = 3.14
+        box.vectors[10] = 3.14
 
         box.add_vectors(10)
-        self.assertEqual(box.vectors.size, 110)
-        self.assertEqual(box.vectors[50], 3.14)
+        self.assertEqual(box.vectors.size, 30)
+        self.assertEqual(box.vectors[10], 3.14)
