ceade572d7aaa4620fd6721002552ec9c89c8e11
chick <chick@qrhino.com>
2015-03-25 14:25:41 -0700

    got heat maps working a little bit

--- hpgmg/finite_volume/main.py
+++ hpgmg/finite_volume/main.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division
+from textwrap import dedent
 
 __author__ = 'nzhang-dev'
 
@@ -12,12 +13,13 @@ from operators.stencil_27_pt import stencil_get_radius
 log = logging
 log.root.setLevel(logging.INFO)
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('log2_box_dim', help='The dimensions of the box taken log 2', default=6, type=int)
     parser.add_argument('target_boxes_per_rank', help='number of boxes per rank', type=int)
     parser.add_argument('-bc', '--boundary-conditions',
-                        help='Type of boundary condition. Use p for Periodic and d for Dirichlet. Default is d or USE_PERIODIC_BC from environment',
+                        help="Type of boundary condition. Use p for Periodic and d for Dirichlet. Default is d",
                         default=('p' if os.environ.get('USE_PERIODIC_BC', 0) else 'd'),
                         choices=['p', 'd'])
     parser.add_argument('-m', '--max_coarse-dim',
@@ -41,12 +43,12 @@ def get_args():
     return args
 
 if __name__ == '__main__':
-    args = get_args()
+    command_line_args = get_args()
 
-    #basic setup
-    box_dim = 1 << args.log2_box_dim
+    # basic setup
+    box_dim = 1 << command_line_args.log2_box_dim
     num_tasks = 1
-    target_boxes = num_tasks * args.target_boxes_per_rank
+    target_boxes = num_tasks * command_line_args.target_boxes_per_rank
     boxes_in_i = -1
     for bi in range(1, 1001):
         total_boxes = bi**3
@@ -54,26 +56,25 @@ if __name__ == '__main__':
             coarse_grid_dim = box_dim*bi
             while coarse_grid_dim % 2 == 0:
                 coarse_grid_dim /= 2
-            if coarse_grid_dim <= args.max_coarse_dim:
+            if coarse_grid_dim <= command_line_args.max_coarse_dim:
                 boxes_in_i = bi
+
+    print("box_dim {} boxes in i-th dimension {}".format(box_dim, boxes_in_i))
 
     if boxes_in_i < 1:
         log.error('failed to find an acceptable problem size')
         exit(0)
 
     ghosts = stencil_get_radius()
-    calc_level = Level(boxes_in_i, box_dim, ghosts,)
+    calc_level = Level(boxes_in_i, box_dim, ghosts, )
 
     #conditional setup for Helmholtz and Poisson
-    if args.eq == 'h':
+    if command_line_args.eq == 'h':
         a = b = 1
         log.info('Creating Helmholtz (a={a}, b={b} test problem)'.format(a=a, b=b))
-    elif args.eq == 'p':
+    elif command_line_args.eq == 'p':
         a = 0
         b = 1
         log.info('Creating Poisson (a={a}, b={b} test problem)'.format(a=a, b=b))
 
     h0 = 1 / (boxes_in_i * box_dim)
-
-
-
