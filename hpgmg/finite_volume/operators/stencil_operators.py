17d462d8eff991d3f22216be5ce14d9938e0d379
Shiv Sundram <shivsundram@berkeley.edu>
2015-04-01 14:33:38 -0700

    ghost regions

--- hpgmg/finite_volume/operators/stencil_operators.py
+++ hpgmg/finite_volume/operators/stencil_operators.py
@@ -13,8 +13,9 @@ class ConstantCoefficent7pt(Operator):
     self.b = b
     self.h2inv = h2inv
 
+
   def apply_op(self, x, i, j, k):
-    jStride = len(x)**(1.0/3)
+    jStride = int(round(pow(len(x),1.0/3)))
     kStride = jStride**2
     ijk = i + j*jStride + k*kStride          
     return self.a*x[ijk] - self.b*self.h2inv*(    \
@@ -27,9 +28,66 @@ class ConstantCoefficent7pt(Operator):
       - x[ijk        ]*6.0         \
     )     
 
+  def D_inv(self, x, i, j, k):
+    #FIX ME. should simply use mesh methods to retrieve number of neighbors
+    s = int(round(pow(len(x),1.0/3)))
+    return 1.0 / (self.a - self.b*self.h2inv*(            \
+             + valid[ijk-1      ]   \
+             + valid[ijk-jStride]   \
+             + valid[ijk-kStride]   \
+             + valid[ijk+1      ]   \
+             + valid[ijk+jStride]   \
+             + valid[ijk+kStride]   \
+             - 12.0                 \
+          ))                        \
+  
+
+def initialize_valid_region(x):
+  dim = int(round(pow(len(x),1.0/3)))
+  jStride = dim
+  kStride = dim**2
+  valid = np.zeros(len(x))
+
+  for ind in range(0, len(x)):
+    valid[ind] = 0
+  for i in range(1, dim-1):
+    for j in range(1, dim-1):
+      for k in range(1, dim-1):
+        ijk = i + j*jStride + k*kStride   
+        valid[ijk] = 1
+  return valid
+
+def add_ghost_regions(x):
+  old_dim = int(round(pow(len(x),1.0/3)))
+  jStride = old_dim
+  kStride = old_dim**2
+  
+  new_dim = old_dim+2
+  new_jStride = new_dim
+  new_kStride = new_dim**2
+
+  new_x = np.zeros(new_dim**3)
+  for i in range(0, old_dim):
+    for j in range(0, old_dim):
+      for k in range(0, old_dim):
+        old_ijk = i + j*jStride + k*kStride 
+        new_ijk =  (1+i) + (1+j)*new_jStride + (1+k)*new_kStride  
+        print old_ijk, new_ijk
+        new_x[new_ijk] = x[old_ijk]
+  return new_x
+
+
 if __name__=="__main__":
 
   xf = np.linspace(0.0, 63.0, num=64)
   A = ConstantCoefficent7pt(1,2, .1)
   print A.apply_op(xf, 1,1,1)
 
+  #print x
+  xf = np.linspace(0.0, 63.0, 64)
+  v = initialize_valid_region(xf)
+  print v
+
+  xf = np.linspace(0.0, 7.0, 8)
+  print xf
+  print add_ghost_regions(xf)
