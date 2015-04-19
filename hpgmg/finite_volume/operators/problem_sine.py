02d2d0f01a0a3f16086687f0cba6767d8f3b55ed
chick <chick@qrhino.com>
2015-04-19 16:38:07 -0700

    create nd beta evaluator move beta generation out of the problems, c ones are the same regardless of which problem it is for

--- hpgmg/finite_volume/operators/problem_sine.py
+++ hpgmg/finite_volume/operators/problem_sine.py
@@ -16,41 +16,6 @@ class SineProblem(Problem):
     beta = 1.0
 
     @staticmethod
-    def evaluate_beta(vector):
-        """
-        :param vector: compute values for beta at point in vector
-        :return:
-        """
-        
-        x, y, z = vector
-
-        b_min = 1.0
-        b_max = 10.0
-        c2 = (b_max-b_min)/2  # coefficients to affect this transition
-        c1 = (b_max+b_min)/2
-        c3 = 10.0  # how sharply (B)eta transitions
-        x_center = 0.50
-        y_center = 0.50
-        z_center = 0.50
-        # calculate distance from center of the domain (0.5, 0.5, 0.5)
-        r2 = (x-x_center)**2 + (y-y_center)**2 + (z-z_center)**2
-        r2x = 2.0*(x-x_center)
-        r2y = 2.0*(y-y_center)
-        r2z = 2.0*(z-z_center)
-
-        r = r2**0.5
-        rx = 0.5 * r2x * (r2 ** -0.5)
-        ry = 0.5 * r2y * (r2 ** -0.5)
-        rz = 0.5 * r2z * (r2 ** -0.5)
-        # -------------------------
-        beta = c1+c2*tanh(c3*(r-0.25))
-        beta_x = c2*c3*rx * (1 - (tanh(c3*(r-0.25))**2))
-        beta_y = c2*c3*ry * (1 - (tanh(c3*(r-0.25))**2))
-        beta_z = c2*c3*rz * (1 - (tanh(c3*(r-0.25))**2))
-
-        return beta, Vector(beta_x, beta_y, beta_z)
-
-    @staticmethod
     def evaluate_u(coord):
         """
         compute the exact value of the function u for a given coordinate
