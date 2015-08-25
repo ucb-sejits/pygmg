50eebc4c1b4b8c0a783c1406cf4afae1c04bde85
Nathan Zhang <nzhang32@berkeley.edu>
2015-08-25 00:44:59 -0700

    remembered v-cycle timing is a mistake

--- hpgmg/finite_volume/simple_hpgmg.py
+++ hpgmg/finite_volume/simple_hpgmg.py
@@ -293,7 +293,6 @@ class SimpleMultigridSolver(object):
         for level in self.all_levels:
             self.smoother.get_kernel(level)
 
-    @time_this
     def v_cycle(self, level, target_mesh, residual_mesh):
         if min(level.space) <= 3:
             with level.timer('total cycles'):
