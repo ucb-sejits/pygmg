63e9fc6741635a4dfad3ca9a8c9d407f17683e27
Nathan Zhang <nzhang32@berkeley.edu>
2015-10-20 13:32:05 -0700

    fixed timings

--- timing/run_tests.sh
+++ timing/run_tests.sh
@@ -4,7 +4,7 @@ mkdir timings
 
 for i in `seq 5 9`;
 do
-    python ../hpgmg/simple_hpgmg.py $i -b c -v -pn p4 -nv 10 -dre &> timings/snowflake_$i.log
+    python ../hpgmg/finite_volume/simple_hpgmg.py $i -b c -v -pn p4 -nv 10 -dre &> timings/snowflake_$i.log
 done
 
 grep "solve.*calls" timings/snowflake_*.log | cut -f 4 > data.out
